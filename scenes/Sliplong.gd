a84ab4161f18cd544a34e6975d6faf8bc226e9f7
Supawee <supawee.ma@kkumail.com>
2026-02-22 21:16:22 +0700

    เมนสองกลับมาแล้ว หายไปไหนมาลูก

--- scenes/Sliplong.gd
+++ scenes/Sliplong.gd
@@ -58,7 +58,7 @@ func spawn_new_receipt():
 	tween.tween_property(new_r, "rect_position:x", 56, 0.7).set_trans(Tween.TRANS_SINE)
 	
 func end_night():
-	print("NIght Complete")
+	get_tree().change_scene_to_file("res://scenes/you_lose.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
