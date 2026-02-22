6ed4af93d7e598ba3842ae9da11608a1c9c6c251
Supawee <supawee.ma@kkumail.com>
2026-02-22 20:24:26 +0700

    หน้าแพ้ หน้าชนะ

---
+++ to_main.gd
@@ -0,0 +1,22 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_pressed():
+	start_game()
+
+func start_game():
+	print("Button was clicked!") # This will show up in the Output log
+	get_tree().change_scene_to_file("res://scenes/game_menu.tscn")
+
+
+func _on_next_night_pressed() -> void:
+	pass # Replace with function body.
